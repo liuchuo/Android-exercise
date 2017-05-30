43e4b6a64879058736803fe3f1a9a5d571a6ebf0
liuchuo <95323362@qq.com>
2017-05-31 07:05:04 +0800

    Add按钮跳转到添加界面

--- ToDoList/app/src/main/java/com/example/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.todolist;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addIntent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(addIntent);
+            }
+        });
     }
 }
